0b103faf85192c39d689c0eea3430febb325c565
Linus Pöppelmann <109509614+lipoeAcc@users.noreply.github.com>
2024-04-04 17:09:13 +0200

    update

---
+++ game-resources/sprites/player1/player1.go
@@ -0,0 +1,23 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"pngegg\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/game-resources/sprites/player1/attack01/attack01.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
