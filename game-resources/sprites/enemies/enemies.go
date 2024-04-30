ef468b51077164d8db4b592aeccfd384c3146575
Linus Pöppelmann <109509614+lipoeAcc@users.noreply.github.com>
2024-04-30 16:49:55 +0200

    zwischenstand
    astar nicht ganz implementiert. Aber ein erster ansatz

---
+++ game-resources/sprites/enemies/enemies.go
@@ -0,0 +1,34 @@
+components {
+  id: "enemies"
+  component: "/game-resources/sprites/enemies/enemies.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "snakefactory"
+  type: "factory"
+  data: "prototype: \"/game-resources/sprites/enemies/1 Snake/snake.go\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
