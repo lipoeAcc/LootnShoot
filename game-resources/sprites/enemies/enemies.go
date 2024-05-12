d7b82fe613f00fb5ca1792de6c692640056e39f4
Linus Pöppelmann <109509614+lipoeAcc@users.noreply.github.com>
2024-05-13 00:15:15 +0200

    healthbar. Und unfertige path function

--- game-resources/sprites/enemies/enemies.go
+++ game-resources/sprites/enemies/enemies.go
@@ -14,9 +14,28 @@ components {
   }
 }
 embedded_components {
-  id: "snakefactory"
+  id: "snakefactory1"
   type: "factory"
   data: "prototype: \"/game-resources/sprites/enemies/1 Snake/snake.go\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "snakefactory"
+  type: "collectionfactory"
+  data: "prototype: \"/game-resources/sprites/enemies/1 Snake/snakeCollection.collection\"\n"
   "load_dynamically: false\n"
   "dynamic_prototype: false\n"
   ""
