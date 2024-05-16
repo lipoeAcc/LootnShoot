4cae5356f5e659334dfdcfa8718874820eaae3de
Nikita Afanasev <na525802@fh-muenster.de>
2024-05-16 17:43:47 +0200

    add mummy to gamefield

---
+++ game-resources/sprites/enemies/5 Mummy/mummy.go
@@ -0,0 +1,72 @@
+embedded_components {
+  id: "mummy_collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  "mass: 1.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"default\"\n"
+  "mask: \"default\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 5.0\n"
+  "      y: -5.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: -0.5299227\n"
+  "      z: 0.0\n"
+  "      w: 0.84804595\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "    id: \"\"\n"
+  "  }\n"
+  "  data: 35.0\n"
+  "  data: 20.0\n"
+  "  data: 0.5\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  "bullet: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "mummy_sprite"
+  type: "sprite"
+  data: "default_animation: \"mummy\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/sprites.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: -0.0050783693
+    y: 0.0
+    z: 0.0
+    w: 0.9999871
+  }
+}
