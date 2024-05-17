569805da70435ca9a9f2e520e352aa6504e0c8e5
Nikita Afanasev <na525802@fh-muenster.de>
2024-05-17 13:58:16 +0200

    mummy moves downwards

--- game-resources/sprites/enemies/5 Mummy/mummy.go
+++ game-resources/sprites/enemies/5 Mummy/mummy.go
@@ -1,3 +1,18 @@
+components {
+  id: "mummy_script"
+  component: "/game-resources/sprites/enemies/5 Mummy/mummy.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "mummy_collisionobject"
   type: "collisionobject"
@@ -6,28 +21,28 @@ embedded_components {
   "mass: 1.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"default\"\n"
-  "mask: \"default\"\n"
+  "group: \"mummy_script\"\n"
+  "mask: \"wall\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
-  "      x: 5.0\n"
-  "      y: -5.0\n"
+  "      x: 15.0\n"
+  "      y: -15.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
   "      x: 0.0\n"
-  "      y: -0.5299227\n"
+  "      y: 0.0\n"
   "      z: 0.0\n"
-  "      w: 0.84804595\n"
+  "      w: 1.0\n"
   "    }\n"
   "    index: 0\n"
   "    count: 3\n"
   "    id: \"\"\n"
   "  }\n"
-  "  data: 35.0\n"
-  "  data: 20.0\n"
+  "  data: 30.0\n"
+  "  data: 32.5\n"
   "  data: 0.5\n"
   "}\n"
   "linear_damping: 0.0\n"
@@ -61,12 +76,17 @@ embedded_components {
   position {
     x: 0.0
     y: 0.0
-    z: 0.0
+    z: 1.0
   }
   rotation {
-    x: -0.0050783693
+    x: 0.0
     y: 0.0
     z: 0.0
-    w: 0.9999871
+    w: 1.0
+  }
+  scale {
+    x: 2.0
+    y: 2.0
+    z: 1.0
   }
 }
