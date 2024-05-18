400cec35d97d06d57d353f35a64752e852e7bdcd
Linus Pöppelmann <109509614+lipoeAcc@users.noreply.github.com>
2024-05-18 14:06:29 +0200

    sword update
    enables hits to multiple targets

--- game-resources/sprites/weapons/melee/sword.go
+++ game-resources/sprites/weapons/melee/sword.go
@@ -14,10 +14,10 @@ components {
   }
 }
 embedded_components {
-  id: "swordcollision"
+  id: "sword_collision"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
-  "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
   "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
@@ -88,5 +88,54 @@ embedded_components {
     x: 0.15
     y: 0.15
     z: 1.0
+  }
+}
+embedded_components {
+  id: "sword_trigger"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"sword\"\n"
+  "mask: \"enemy\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "    id: \"\"\n"
+  "  }\n"
+  "  data: 25.0\n"
+  "  data: 25.0\n"
+  "  data: 25.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  "bullet: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
   }
 }
