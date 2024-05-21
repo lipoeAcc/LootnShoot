110d5cc4db5affccfb59829e5662de1d0c3604e4
Linus Pöppelmann <109509614+lipoeAcc@users.noreply.github.com>
2024-05-21 18:08:30 +0200

    init

---
+++ main/loader/loader.go
@@ -0,0 +1,18 @@
+embedded_components {
+  id: "collectionproxy"
+  type: "collectionproxy"
+  data: "collection: \"/main/level/level_1/level_1.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
