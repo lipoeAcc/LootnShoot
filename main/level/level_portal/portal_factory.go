f43368dc7a1f1692e59758060db3e5715a3178f2
Linus Pöppelmann <109509614+lipoeAcc@users.noreply.github.com>
2024-05-22 23:37:52 +0200

    portal

---
+++ main/level/level_portal/portal_factory.go
@@ -0,0 +1,19 @@
+embedded_components {
+  id: "collectionfactory"
+  type: "collectionfactory"
+  data: "prototype: \"/main/level/level_portal/level_portal.collection\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
