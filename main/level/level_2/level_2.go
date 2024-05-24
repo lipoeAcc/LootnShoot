cc8617e189bacf98817329d8b308f606cf9b0165
Linus Pöppelmann <109509614+lipoeAcc@users.noreply.github.com>
2024-05-24 18:16:51 +0200

    player_health

--- main/level/level_2/level_2.go
+++ main/level/level_2/level_2.go
@@ -28,3 +28,31 @@ components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"/main/level/level_2/level_2.tilemap\"\n"
+  "type: COLLISION_OBJECT_TYPE_STATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"\"\n"
+  "mask: \"player\"\n"
+  "mask: \"mummy\"\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  "bullet: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
