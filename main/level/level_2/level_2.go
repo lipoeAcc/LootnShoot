a5791b07c69448cf7c564a89aa504dc0b90fc62a
Linus Pöppelmann <109509614+lipoeAcc@users.noreply.github.com>
2024-05-24 17:53:30 +0200

    level_loader update

---
+++ main/level/level_2/level_2.go
@@ -0,0 +1,30 @@
+components {
+  id: "level_2"
+  component: "/main/level/level_2/level_2.tilemap"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "level_2_script"
+  component: "/main/level/level_2/level_2_script.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- main/loader/loader.go
+++ main/loader/loader.go
@@ -49,3 +49,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "level_2_proxy"
+  type: "collectionproxy"
+  data: "collection: \"/main/level/level_2/level_2.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
