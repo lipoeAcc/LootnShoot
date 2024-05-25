f771b89f1068440878efb06db8cc5842a5035dd6
Linus Pöppelmann <109509614+lipoeAcc@users.noreply.github.com>
2024-05-25 16:03:26 +0200

    more items

--- main/items/item_factory.go
+++ main/items/item_factory.go
@@ -32,3 +32,60 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "melee_attack_speed_factory"
+  type: "collectionfactory"
+  data: "prototype: \"/main/items/melee_attack_speed/melee_attack_speed.collection\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "ranged_attack_speed_factory"
+  type: "collectionfactory"
+  data: "prototype: \"/main/items/ranged_attack_speed/ranged_attack_speed.collection\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "movement_speed_factory"
+  type: "collectionfactory"
+  data: "prototype: \"/main/items/movement_speed/movement_speed.collection\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
