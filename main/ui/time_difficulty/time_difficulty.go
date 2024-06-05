7958c09d0e43a318c6a380dddf7cbdb7b02faab0
Linus Pöppelmann <linus.poeppelmann@loyjoy.com>
2024-06-05 14:07:41 +0200

    upd

---
+++ main/ui/time_difficulty/time_difficulty.go
@@ -0,0 +1,15 @@
+components {
+  id: "timer_difficulty"
+  component: "/main/ui/time_difficulty/time_difficulty.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
