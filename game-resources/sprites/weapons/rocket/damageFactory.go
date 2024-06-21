d610f9220317bd126c214c701390399d4dbfa5e6
Linus Pöppelmann <109509614+lipoeAcc@users.noreply.github.com>
2024-06-22 01:07:33 +0200

    aoe update

---
+++ game-resources/sprites/weapons/rocket/damageFactory.go
@@ -0,0 +1,19 @@
+embedded_components {
+  id: "damage_factory"
+  type: "factory"
+  data: "prototype: \"/game-resources/sprites/weapons/rocket/damageradius.go\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- game-resources/sprites/weapons/rocket/damageradius.go
+++ game-resources/sprites/weapons/rocket/damageradius.go
@@ -1,33 +1,10 @@
 components {
-  id: "snake"
-  component: "/game-resources/sprites/enemies/1 Snake/snake.script"
+  id: "damageradius"
+  component: "/game-resources/sprites/weapons/rocket/damageradius.script"
   position {
     x: 0.0
     y: 0.0
     z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
-  id: "sprite"
-  type: "sprite"
-  data: "default_animation: \"snake_walk\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  "textures {\n"
-  "  sampler: \"texture_sampler\"\n"
-  "  texture: \"/main/sprites.atlas\"\n"
-  "}\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 1.0
   }
   rotation {
     x: 0.0
@@ -44,14 +21,11 @@ embedded_components {
   "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"enemy\"\n"
-  "mask: \"rockets\"\n"
-  "mask: \"sword\"\n"
-  "mask: \"player\"\n"
-  "mask: \"damageradius\"\n"
+  "group: \"damageradius\"\n"
+  "mask: \"enemy\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
-  "    shape_type: TYPE_BOX\n"
+  "    shape_type: TYPE_SPHERE\n"
   "    position {\n"
   "      x: 0.0\n"
   "      y: 0.0\n"
@@ -64,12 +38,10 @@ embedded_components {
   "      w: 1.0\n"
   "    }\n"
   "    index: 0\n"
-  "    count: 3\n"
+  "    count: 1\n"
   "    id: \"\"\n"
   "  }\n"
-  "  data: 15.0\n"
-  "  data: 6.5\n"
-  "  data: 10.0\n"
+  "  data: 25.0\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
