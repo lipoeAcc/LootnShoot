2327fa4a69c72ac2bb2969f7e2afe5b228857008
Linus Pöppelmann <109509614+lipoeAcc@users.noreply.github.com>
2024-06-23 22:25:43 +0200

    enable_scorpio spawn

--- game-resources/sprites/enemies/enemies.go
+++ game-resources/sprites/enemies/enemies.go
@@ -14,9 +14,9 @@ components {
   }
 }
 embedded_components {
-  id: "snakefactory1"
-  type: "factory"
-  data: "prototype: \"/game-resources/sprites/enemies/1 Snake/snake.go\"\n"
+  id: "snakefactory"
+  type: "collectionfactory"
+  data: "prototype: \"/game-resources/sprites/enemies/1 Snake/snakeCollection.collection\"\n"
   "load_dynamically: false\n"
   "dynamic_prototype: false\n"
   ""
@@ -33,9 +33,9 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "snakefactory"
+  id: "scorpiofactory"
   type: "collectionfactory"
-  data: "prototype: \"/game-resources/sprites/enemies/1 Snake/snakeCollection.collection\"\n"
+  data: "prototype: \"/game-resources/sprites/enemies/3 Scorpio/scorpio.collection\"\n"
   "load_dynamically: false\n"
   "dynamic_prototype: false\n"
   ""
